e687391f563d1087b230aa86225a99127efa4cf8
Heinz Mayer <heinz@mic.at>
2023-04-27 09:28:00 +0200

    Update k6test.ts

--- k6/k6test.ts
+++ k6/k6test.ts
@@ -2,23 +2,21 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Rate } from 'k6/metrics';
 import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.2/index.js';
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 export const errorRate = new Rate('errors');
 
 export const options = {
     discardResponseBodies: true,
     scenarios: {
-      mic: {
+      mic_demo: {
         executor: 'constant-vus',
         vus: 100,
-        duration: '20s',
+        duration: '120s',
       },
     },
   };
 
 export default function () {
-  const randomUUID = uuidv4();
 
   const params = {
     timeout: '120s',
@@ -30,11 +28,8 @@ export default function () {
   var url = 'http://172.21.122.249/';
 
   const ReqBody = `vote=Blue`;
-
   
   var response = http.post(url, ReqBody, params);
-
-
 
   check(response, {
     "status code should be 200": res => res.status === 200,
